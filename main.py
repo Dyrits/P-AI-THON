23c46c9f195552a49b9cc4368ea97f25100b2f55
Dylan J. Gerrits <gerritsdylan@gmail.com>
2025-11-01 10:05:36 +0100

    >> CH2L2: Get Files

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+arguments = os.sys.argv
+if len(arguments) < 2:
+    print("Usage: python main.py '<your prompt here>'")
+    exit(1)
+
+prompt = arguments[1]
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
 
 load_dotenv()
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model="gemini-2.0-flash-001", contents=messages
 )
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in arguments:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
