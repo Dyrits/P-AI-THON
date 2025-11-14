f87bf2deb6994d3e0a137e6be901725f8d1d1cfc
Dylan J. Gerrits <gerritsdylan@gmail.com>
2025-11-14 18:48:31 +0100

    >> CH3L1: System Prompt

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ messages = [
 
 load_dotenv()
 
+system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=messages
+    model="gemini-2.0-flash-001", contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
 )
 
 print(response.text)
